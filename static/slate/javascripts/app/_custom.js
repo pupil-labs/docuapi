0a7b2c529c24ec34a14c40b0f3c45beb5325a1d5
Nathakit <nathakit.sae-tan@cuinda.com>
2017-06-13 18:55:39 +0700

    structuring github links for headers

--- static/slate/javascripts/app/_custom.js
+++ static/slate/javascripts/app/_custom.js
@@ -6,6 +6,14 @@ $(document).ready(function() {
   $('.github-link').each(function() {
       //$(this).append("<a href='"+$(this).data('github')+"' rel='noopener'>Edit `"+$(this).data('file')+"` on Github</a>");
       //$(this).insertAfter($(this).nextAll(':header').filter(':first'));
+
+      // remove .filter line
+      // update the attr line with below
+      // we already know the hash of the anchor from tocify
+      // and we assume that this kebab case representation matches
+      // github's anchor link representation (e.g. Section name == section-name)
+
+      // $(this).data('github').concat($(this).data('hash')
       $(this).nextAll(':header')
         .filter(':first')
         .find('.github-edit')
@@ -13,10 +21,31 @@ $(document).ready(function() {
         .append("<i class='material-icons'>mode_edit</i>");
   });
 
+var github_links = document.getElementByClassName('github_links')
+
+for (var i = 0; i < github_links.length; i++) {
+
+    var source_code_link = $(github_links).data('github');
+    var headers = $(github_links[i]).nextUntil($(github_links[i+1]),'h1,h2,h3');
+
+    console.log("section number: "+i+"\nnum headers: "+headers.length+"\nheaders: "+headers);
+
+    $(headers).each(function(){
+      var item = $(this).find(".github-edit")
+      var url = source_code_link.concat($(item).data('hash'));
+
+      console.log("url: "+url);
+      
+      $(item).attr('href',url);
+    });
+}
+
+
+
   $(".anchor").click( function() {
-  	event.preventDefault();
-  	history.replaceState(null, null, $(this).attr("href"));
-  	$(window).scrollTop($("div[data-unique='"+$(this).attr("name")+"']").offset().top - 49);
+    event.preventDefault();
+    history.replaceState(null, null, $(this).attr("href"));
+    $(window).scrollTop($("div[data-unique='"+$(this).attr("name")+"']").offset().top - 49);
   })
 
   $("#version-selector").change(function() {
