cdc151da821dffced0d273a08e11fff7d6ee7cb8
Nathakit <nathakit.sae-tan@cuinda.com>
2017-06-15 11:09:13 +0700

    update github edit links for all headers

--- static/slate/javascripts/app/_custom.js
+++ static/slate/javascripts/app/_custom.js
@@ -3,45 +3,6 @@ $(document).ready(function() {
   // on document load - version selector should show current version
   $("#version-selector").val(window.location.pathname.replace(/\//g,''));
 
-  $('.github-link').each(function() {
-      //$(this).append("<a href='"+$(this).data('github')+"' rel='noopener'>Edit `"+$(this).data('file')+"` on Github</a>");
-      //$(this).insertAfter($(this).nextAll(':header').filter(':first'));
-
-      // remove .filter line
-      // update the attr line with below
-      // we already know the hash of the anchor from tocify
-      // and we assume that this kebab case representation matches
-      // github's anchor link representation (e.g. Section name == section-name)
-
-      // $(this).data('github').concat($(this).data('hash')
-      $(this).nextAll(':header')
-        .filter(':first')
-        .find('.github-edit')
-        .attr('href', $(this).data('github'))
-        .append("<i class='material-icons'>mode_edit</i>");
-  });
-
-var github_links = document.getElementByClassName('github_links')
-
-for (var i = 0; i < github_links.length; i++) {
-
-    var source_code_link = $(github_links).data('github');
-    var headers = $(github_links[i]).nextUntil($(github_links[i+1]),'h1,h2,h3');
-
-    console.log("section number: "+i+"\nnum headers: "+headers.length+"\nheaders: "+headers);
-
-    $(headers).each(function(){
-      var item = $(this).find(".github-edit")
-      var url = source_code_link.concat($(item).data('hash'));
-
-      console.log("url: "+url);
-      
-      $(item).attr('href',url);
-    });
-}
-
-
-
   $(".anchor").click( function() {
     event.preventDefault();
     history.replaceState(null, null, $(this).attr("href"));
@@ -56,3 +17,31 @@ for (var i = 0; i < github_links.length; i++) {
   });
 
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+
+  var elements = document.getElementsByClassName('github-link');
+
+  for (var i = 0; i < elements.length; i++) {
+    var _e = elements[i];
+    var section_link = _e.getAttribute('data-github');
+
+    while (_e.nextElementSibling && !_e.nextElementSibling.matches('.github-link')) {
+      if (_e.matches("h1") || _e.matches("h2") || _e.matches("h3")){
+        // the h1,h2,h3 nodes have text and two links as children
+        // child_nodes[1] is the first link (hash link to the section)
+        // child_nodes[2] or lastChild is the external link to github
+        var child_nodes = _e.childNodes;
+        var heading_hash = child_nodes[1].getAttribute('href');
+        var hash_link = section_link + heading_hash;
+        var github_edit_link = _e.lastChild;
+        github_edit_link.setAttribute('href',hash_link);
+        github_edit_link.insertAdjacentHTML('afterbegin','<i class="material-icons">mode_edit</i>');
+      }
+      _e = _e.nextElementSibling;
+    }
+
+  }
+
+});
+
