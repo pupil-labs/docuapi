ca64166599abca2728f3dcee06ad206ed078053c
Nathakit <nathakit.sae-tan@cuinda.com>
2017-08-08 13:57:53 +0700

    fix regex and if statement

--- static/slate/javascripts/app/_youtube-lazyload.js
+++ static/slate/javascripts/app/_youtube-lazyload.js
@@ -18,15 +18,15 @@
         var iframe = document.createElement("iframe");
 
         var iframe_embed = this.dataset.embed.replace(/h.+ed.|[?].+/g,'');
-        var embed_playlist = this.dataset.embed.replace(/h.+ed.+[?]/g,'');
+        var embed_playlist = this.dataset.embed.match(/[?].+/g,'');
 
         iframe.setAttribute("frameborder", "0");
         iframe.setAttribute("allowfullscreen", "");
 
-        if ( embed_playlist === "" ) {
+        if ( embed_playlist === null ) {
           iframe.setAttribute("src", "https://www.youtube.com/embed/"+ iframe_embed + "?rel=0&showinfo=0&autoplay=1");
         } else {
-          iframe.setAttribute("src", "https://www.youtube.com/embed/"+ iframe_embed + "?" + embed_playlist + "&rel=0&showinfo=1&autoplay=1&loop=1");
+          iframe.setAttribute("src", "https://www.youtube.com/embed/"+ iframe_embed + embed_playlist + "&rel=0&showinfo=1&autoplay=1&loop=1");
         }
 
         this.innerHTML = "";
